a353d017d34749e7f65bc7ca3c28c8d59c3c99ba
xiezhyan <1367523+xiezhyan@users.noreply.gitee.com>
2019-02-25 20:57:37 +0800

    no commit message

--- Redis/src/main/java/com/sanq/product/redis/service/JedisPoolService.java
+++ Redis/src/main/java/com/sanq/product/redis/service/JedisPoolService.java
@@ -8,37 +8,128 @@ import java.util.Set;
  */
 public interface JedisPoolService {
 
+    /**
+     * 保存字符串
+     * @param key
+     * @param val
+     * @return
+     */
     boolean set(String key, String val);
 
+    /**
+     * 保存字符串
+     * @param key
+     * @param val
+     * @param seconds   有效时间
+     * @return
+     */
     boolean set(String key, String val, int seconds);
 
+    /**
+     * 获取字符串
+     * @param key
+     * @return
+     */
     String get(String key);
 
+    /**
+     * 删除
+     * @param key
+     * @return
+     */
     boolean delete(String...key);
 
+    /**
+     * 自增
+     * @param key
+     * @return
+     */
     long incr(String key);
 
+    /**
+     * 自增
+     * @param key
+     * @param max   从max开始
+     * @return
+     */
     long incr(String key, int max);
 
+    /**
+     * 判断redis中是否存在
+     * @param key
+     * @return
+     */
     boolean exists(String key);
 
+    /**
+     * 批量获取key
+     * @param pattern
+     * @return
+     */
     Set<String> keys(String pattern);
 
+    /**
+     * 设置过期时间
+     * @param key
+     * @param seconds
+     * @return
+     */
     boolean expire(String key, int seconds);
 
     /**************************华丽丽的分割线**************************************/
+    /**
+     * 保存到List
+     * @param key
+     * @param value
+     * @return
+     */
     long putList(String key, String value);
 
+    /**
+     * 移除List
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
     boolean rmList(String key, long start, long end);
 
+    /**
+     * 获取List
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
     List<String> getList(String key, long start, long end);
 
+    /**
+     * 获取List长度
+     * @param key
+     * @return
+     */
     long llen(String key);
 
     /**************************华丽丽的分割线**************************************/
 
+    /**
+     * 保存到有序set
+     * @param key
+     * @param score
+     * @param value
+     * @return
+     */
     boolean putSet(String key, double score, String value);
 
+    /**
+     * 获取set
+     * @param key
+     * @param start
+     * @param end
+     * @param order   ASC DESC
+     *
+     * @return
+     */
     Set<String> getSet(String key, long start, long end, String order);
 
     /**
@@ -67,19 +158,52 @@ public interface JedisPoolService {
 
     /**************************华丽丽的分割线**************************************/
 
-    //不标准去重统计  100000  -->  443
+    /**
+     * 添加到统计
+     * @param key
+     * @param value
+     * @return
+     */
     boolean pfAdd(String key, String...value);
 
+    /**
+     * 获取统计数量
+     * @param key
+     * @return
+     */
     long pfCount(String key);
 
-    //位图
+    /**
+     * 位图
+     * @param key
+     * @param offset
+     * @param value
+     * @return
+     */
     boolean setBit(String key, long offset, boolean value);
 
+    /**
+     * 获取位图
+     * @param key
+     * @param offset
+     * @return
+     */
     boolean getBit(String key, long offset);
 
-    //执行lua脚本
+    /**
+     * 执行lua脚本
+     * @param script
+     * @param keys
+     * @param args
+     * @return
+     */
     boolean eval(String script, List<String> keys, List<String> args);
 
+    /**
+     * 批量删除key
+     * @param pattern
+     * @return
+     */
     boolean deletes(String pattern);
 
 
