515add3a6d3c0c72d3d480c2477f441ea16bb84a
xiezhyan <1367523+xiezhyan@users.noreply.gitee.com>
2019-03-23 21:34:02 +0800

    no commit message

--- Common/src/main/java/com/sanq/product/config/utils/string/MatcherUtil.java
+++ Common/src/main/java/com/sanq/product/config/utils/string/MatcherUtil.java
@@ -18,7 +18,7 @@ public class MatcherUtil {
      * @return 如果是符合的字符串,返回 <b>true </b>,否则为 <b>false </b>
      */
     public static boolean isEmail(String str) {
-        String regex = "^([\\w-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([\\w-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+        String regex = "/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$/";
         return match(regex, str);
     }
 
