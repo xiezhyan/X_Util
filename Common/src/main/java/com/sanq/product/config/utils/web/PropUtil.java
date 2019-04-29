25846732d8e7a75c2f7955dd45ae31c1ab454b72
xiezhyan <bin_cruel@sohu.com>
2019-04-29 15:18:07 +0800

    修改PropUtil， 新增修改

--- Common/src/main/java/com/sanq/product/config/utils/web/PropUtil.java
+++ Common/src/main/java/com/sanq/product/config/utils/web/PropUtil.java
@@ -13,9 +13,10 @@ public class PropUtil {
     private static Properties properties;
 
     static {
-        loadProp();
+        //loadProp();
     }
 
+    @Deprecated
     synchronized static private void loadProp() {
         properties = new Properties();
         InputStream in = null;
@@ -37,6 +38,28 @@ public class PropUtil {
         }
     }
 
+    synchronized static private void loadProp(String path) {
+        properties = new Properties();
+        InputStream in = null;
+        try {
+            in = new FileInputStream(path);
+            properties.load(in);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(null != in) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Deprecated
     public static String getProperty(String key){
         if(null == properties) {
             loadProp();
@@ -44,6 +67,7 @@ public class PropUtil {
         return properties.getProperty(key);
     }
 
+    @Deprecated
     public static String getProperty(String key, String defaultValue) {
         if(null == properties) {
             loadProp();
@@ -51,4 +75,32 @@ public class PropUtil {
         return properties.getProperty(key, defaultValue);
     }
 
+
+    public static String getProperty(String path, String key){
+        if(null == properties) {
+            loadProp(path);
+        }
+        return properties.getProperty(key);
+    }
+
+	public static void saveOrUpdate(String path, String key, String value) {
+        if(null == properties) {
+            loadProp(path);
+        }
+
+		properties.setProperty(key, value);
+		FileOutputStream oFile = null;
+		try {
+			oFile = new FileOutputStream(path);
+			properties.store(oFile, "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				oFile.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
