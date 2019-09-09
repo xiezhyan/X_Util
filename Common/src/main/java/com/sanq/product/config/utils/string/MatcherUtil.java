441bd9e724fc995a1c5c140de8e5476dc2fe1932
xiezhyan <bin_cruel@sohu.com>
2019-09-09 18:10:14 +0800

    no commit message

--- Common/src/main/java/com/sanq/product/config/utils/string/MatcherUtil.java
+++ Common/src/main/java/com/sanq/product/config/utils/string/MatcherUtil.java
@@ -11,248 +11,210 @@ import java.util.regex.Pattern;
  */
 
 public class MatcherUtil {
+    public static final String EMAIL_REGEX = "/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$/";
+    
+    private static final String IP_NUM = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
+    public static final String IP_REGEX = "^" + IP_NUM + "\\." + IP_NUM + "\\." + IP_NUM + "\\." + IP_NUM + "$";
+    
+    public static final String URL_REGEX = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+    
+    public static final String PHONE_REGEX = "^(\\d{3,4}-)?\\d{6,8}$";
+
+    public static final String PASSWORD_REGEX = "(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{6,}";
+
+    public static final String POSTAL_CODE_REGEX = "^\\d{6}$";
+
+    public static final String TEL_REGEX = "^[1](([3][0-9])|([4][5,7,9])|([5][^4,6,9])|([6][6])|([7][3,5,6,7,8])|([8][0-9])|([9][8,9]))[0-9]{8}$";
+
+    public static final String NUMBER_REGEX = "^[0-9]*$";
+
+    public static final String INT_REGEX = "^\\+?[1-9][0-9]*$";
+
+    public static final String UP_CHAR_REGEX = "^[A-Z]+$";
+
+    public static final String LOW_CHAR_REGEX = "^[a-z]+$";
+
+    public static final String LETTER_REGEX = "^[A-Za-z]+$";
+
+    public static final String CHINESE_REGEX = "^[\u4e00-\u9fa5],{0,}$";
+
+
+
+    
     /**
      * 验证邮箱
      *
-     * @param 待验证的字符串
-     * @return 如果是符合的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
     public static boolean isEmail(String str) {
-        String regex = "/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$/";
+        String regex = EMAIL_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证IP地址
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
     public static boolean isIP(String str) {
-        String num = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
-        String regex = "^" + num + "\\." + num + "\\." + num + "\\." + num + "$";
+        String regex = IP_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证网址Url
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsUrl(String str) {
-        String regex = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+    public static boolean isUrl(String str) {
+        String regex = URL_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证电话号码
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsPhone(String str) {
-        String regex = "^(\\d{3,4}-)?\\d{6,8}$";
+    public static boolean isPhone(String str) {
+        String regex = PHONE_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证输入密码条件(字符与数据同时出现)
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsPassword(String str) {
-        String regex = "(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{8,}";
-        return match(regex, str);
-    }
-
-    /**
-     * 验证输入密码长度 (6-18位)
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsPasswLength(String str) {
-        String regex = "^\\d{6,18}$";
+    public static boolean isPassword(String str) {
+        String regex = PASSWORD_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证输入邮政编号
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsPostalcode(String str) {
-        String regex = "^\\d{6}$";
+    public static boolean isPostalcode(String str) {
+        String regex = POSTAL_CODE_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证输入手机号码
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsTelephone(String str) {
-        String regex = "^[1](([3][0-9])|([4][5,7,9])|([5][^4,6,9])|([6][6])|([7][3,5,6,7,8])|([8][0-9])|([9][8,9]))[0-9]{8}$";
+    public static boolean isTelephone(String str) {
+        String regex = TEL_REGEX;
         return match(regex, str);
     }
 
     public static void main(String[] args) {
-        System.out.println(IsTelephone("15110148609"));
+        System.out.println(isTelephone("15110148609"));
     }
 
-    /**
-     * 验证输入身份证号
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsIDcard(String str) {
-        String regex = "(^\\d{18}$)|(^\\d{15}$)";
-        return match(regex, str);
-    }
-
-    /**
-     * 验证输入两位小数
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsDecimal(String str) {
-        String regex = "^[0-9]+(.[0-9]{2})?$";
-        return match(regex, str);
-    }
-
-    /**
-     * 验证输入一年的12个月
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsMonth(String str) {
-        String regex = "^(0?[[1-9]|1[0-2])$";
-        return match(regex, str);
-    }
-
-    /**
-     * 验证输入一个月的31天
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsDay(String str) {
-        String regex = "^((0?[1-9])|((1|2)[0-9])|30|31)$";
-        return match(regex, str);
-    }
-
-    /**
-     * 验证日期时间
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合网址格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean isDate(String str) {
-// 严格验证时间格式的(匹配[2002-01-31], [1997-04-30],
-// [2004-01-01])不匹配([2002-01-32], [2003-02-29], [04-01-01])
-// String regex =
-// "^((((19|20)(([02468][048])|([13579][26]))-02-29))|((20[0-9][0-9])|(19[0-9][0-9]))-((((0[1-9])|(1[0-2]))-((0[1-9])|(1\\d)|(2[0-8])))|((((0[13578])|(1[02]))-31)|(((01,3-9])|(1[0-2]))-(29|30)))))$";
-// 没加时间验证的YYYY-MM-DD
-// String regex =
-// "^((((1[6-9]|[2-9]\\d)\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\d|3[01]))|(((1[6-9]|[2-9]\\d)\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\d|30))|(((1[6-9]|[2-9]\\d)\\d{2})-0?2-(0?[1-9]|1\\d|2[0-8]))|(((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))$";
-// 加了时间验证的YYYY-MM-DD 00:00:00
-        String regex = "^((((1[6-9]|[2-9]\\d)\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\d|3[01]))|(((1[6-9]|[2-9]\\d)\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\d|30))|(((1[6-9]|[2-9]\\d)\\d{2})-0?2-(0?[1-9]|1\\d|2[0-8]))|(((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\\d):[0-5]?\\d:[0-5]?\\d$";
-        return match(regex, str);
-    }
 
     /**
      * 验证数字输入
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsNumber(String str) {
-        String regex = "^[0-9]*$";
+    public static boolean isNumber(String str) {
+        String regex = NUMBER_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证非零的正整数
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsIntNumber(String str) {
-        String regex = "^\\+?[1-9][0-9]*$";
+    public static boolean isIntNumber(String str) {
+        String regex = INT_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证大写字母
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsUpChar(String str) {
-        String regex = "^[A-Z]+$";
+    public static boolean isUpChar(String str) {
+        String regex = UP_CHAR_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证小写字母
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsLowChar(String str) {
-        String regex = "^[a-z]+$";
+    public static boolean isLowChar(String str) {
+        String regex = LOW_CHAR_REGEX;
         return match(regex, str);
     }
 
     /**
-     * 验证验证输入字母
+     * 验证输入字母
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsLetter(String str) {
-        String regex = "^[A-Za-z]+$";
+    public static boolean isLetter(String str) {
+        String regex = LETTER_REGEX;
         return match(regex, str);
     }
 
     /**
      * 验证验证输入汉字
      *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
+     * @return 如果是符合格式的字符串, 返回 <b>true </b>,否则为 <b>false </b>
      */
-    public static boolean IsChinese(String str) {
-        String regex = "^[\u4e00-\u9fa5],{0,}$";
+    public static boolean isChinese(String str) {
+        String regex = CHINESE_REGEX;
         return match(regex, str);
     }
 
     /**
-     * 验证验证输入字符串
-     *
-     * @param 待验证的字符串
-     * @return 如果是符合格式的字符串,返回 <b>true </b>,否则为 <b>false </b>
-     */
-    public static boolean IsLength(String str) {
-        String regex = "^.{8,}$";
-        return match(regex, str);
-    }
-    /**
-     * @param regex
-     * 正则表达式字符串
-     * @param str
-     * 要匹配的字符串
+     * @param regex 正则表达式字符串
+     * @param str   要匹配的字符串
      * @return 如果str 符合 regex的正则表达式格式,返回true, 否则返回 false;
      */
     public static boolean match(String regex, String str) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
         return matcher.matches();
+    }
+
+    /**
+     * 是否包含
+     *
+     * @param regex
+     * @param str
+     * @return
+     */
+    public static boolean find(String regex, String str) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.find();
+    }
+
+    /**
+     * 得到匹配到的内容
+     *
+     * @param regex
+     * @param str
+     * @param group
+     * @return
+     */
+    public static String group(String regex, String str, int group) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            return matcher.group(group);
+        }
+        return "";
     }
 }
