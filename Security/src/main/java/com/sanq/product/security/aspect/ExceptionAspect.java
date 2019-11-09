186fa3804e0b1cdb945e0243196676e6662adce4
xiezhyan <bin_cruel@sohu.com>
2019-11-09 21:09:14 +0800

    修改

--- Security/src/main/java/com/sanq/product/security/aspect/ExceptionAspect.java
+++ Security/src/main/java/com/sanq/product/security/aspect/ExceptionAspect.java
@@ -5,22 +5,14 @@ import com.sanq.product.config.utils.auth.exception.NoParamsException;
 import com.sanq.product.config.utils.auth.exception.TokenException;
 import com.sanq.product.config.utils.entity.Response;
 import com.sanq.product.config.utils.entity.ResultCode;
-import com.sanq.product.config.utils.web.JsonUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @ControllerAdvice   // 控制器增强
 @ResponseBody
@@ -82,6 +74,7 @@ public class ExceptionAspect {
 
     /**
      * 授权
+     *
      * @param e
      * @return
      */
