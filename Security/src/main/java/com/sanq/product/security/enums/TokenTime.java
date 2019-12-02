c975ee57dff67dc70dc330158a131e4be570c8c4
xiezhyan <bin_cruel@sohu.com>
2019-12-02 10:38:34 +0800

    添加随机时间

--- Security/src/main/java/com/sanq/product/security/enums/TokenTime.java
+++ Security/src/main/java/com/sanq/product/security/enums/TokenTime.java
@@ -1,8 +1,27 @@
 package com.sanq.product.security.enums;
 
+import org.apache.commons.lang3.RandomUtils;
+
 public enum  TokenTime {
 
-    TOKEN_TIME(10 * 60);
+    /**
+     * 10分钟
+     */
+    TOKEN_TIME(10 * 60),    //10分钟
+    /**
+     * 30分钟
+     */
+    THIRTY_MINUTES(30 * 60),
+
+    /**
+     * 5分钟
+     */
+    FIVE_MINUTES(5 * 60),
+
+    /**
+     * 2分钟
+     */
+    TWO_MINUTES(2 * 60);
 
     private int time;
 
@@ -11,6 +30,6 @@ public enum  TokenTime {
     }
 
     public int getTime() {
-        return time;
+        return time + RandomUtils.nextInt(0, 300);
     }
 }
